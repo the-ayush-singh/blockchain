c1cc2a9d423c36b10e9c5fc8139b4c606d7ad589
Ayush Singh <19u01045@iiitbhopal.ac.in>
2021-04-22 23:15:15 +0530

    Created a Hash Function using SHA256 of crypto-js module

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256');
 class Block{
     constructor(timestamp, lastHash, hash, data){
         this.timestamp = timestamp;
@@ -14,6 +15,17 @@ class Block{
     }
     static genesis() {
         return new this('Genesis Time', '----------', 'fX4d4h5s3', 'Genesis');
+    }
+    static mineBlock(lastBlock, data){
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data);
+
+    }
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 // ====================================Testing ===============================
